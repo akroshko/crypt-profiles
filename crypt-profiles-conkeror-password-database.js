2ea9ff8abb97149e8e35086c3f567ac32737f786
Andrew Kroshko <akroshko.public+devel@gmail.com>
2019-02-28 13:44:32 -0600

    Version bump all.

--- crypt-profiles-conkeror-password-database.js
+++ crypt-profiles-conkeror-password-database.js
@@ -5,7 +5,7 @@
 // Author: Andrew Kroshko
 // Maintainer: Andrew Kroshko <akroshko.public+devel@gmail.com>
 // Created: Mon Jun 20, 2016
-// Version: 20190115
+// Version: 20190228
 // URL: https://github.com/akroshko/crypt-profiles
 //
 // This program is free software; you can redistribute it and/or
